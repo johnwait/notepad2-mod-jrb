3fdb551552fc13877575afd17f739c73057f2c87
johnwait <jonathan.brochu@gmail.com>
2019-08-19 01:24:54 -0400

    Fix for improper parameter size on 64-bit platforms (only for debug)

--- onigmo/regcomp.c
+++ onigmo/regcomp.c
@@ -30,6 +30,11 @@
 
 #include "regparse.h"
 
+#if defined(__LP64__) || defined(_WIN64) // if 64 bit environment
+#define FMT_LONG_T "lld"
+#else
+#define FMT_LONG_T "ld"
+#endif
 OnigCaseFoldType OnigDefaultCaseFoldFlag = ONIGENC_CASE_FOLD_MIN;
 
 extern OnigCaseFoldType
@@ -6446,9 +6451,9 @@ print_compiled_byte_code_list(FILE* f, regex_t* reg)
   while (bp < end) {
     ncode++;
     if (ncode % 5 == 0)
-      fprintf(f, "\n%ld:", bp - reg->p);
+      fprintf(f, "\n%"FMT_LONG_T":", bp - reg->p);
     else
-      fprintf(f, " %ld:", bp - reg->p);
+      fprintf(f, " %"FMT_LONG_T":", bp - reg->p);
     onig_print_compiled_byte_code(f, bp, end, &bp, reg->enc);
   }
 
