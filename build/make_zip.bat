johnwait/notepad2-mod-jrb

commit cc900a62983a294c71c575d0f19728ec5f1c9eb7

Now properly handling and catching regex engine errors
Including:
- Invalid regex patterns (when their compilation fails)
- Execution errors
- Exceptions, in case we catch any

For example, before that, hitting "Replace All" with "regular expression search" ticked and an invalid/incomplete pattern would throw the program into an infinite loop.

Modifications to Scintilla involved:
- In Scintilla.h (normally autogenerated), added:
  + three new return codes to use with SCI_FINDTEXT in addition to INVALID_POSITION;
  + three new SC_STATUS_* codes;
  + a new message, SCI_GETREGEXLASTERROR, to allow the retrieval of Document::GetRegexLastError()' return value (see below);
- Replaced RegexError() with three new & distinctive std::runtime_error() types;
- Added RegexSearchBase::GetErrorInfo(), and exposed it through Document::GetRegexLastError() and Editor::GetRegexLastError();

scintilla/src/Editor.cxx
@@ -4060,14 +4060,30 @@ Sci::Position Editor::FindText(
 			ft->lpstrText,
 			static_cast<int>(wParam),
 			&lengthFound);
-		if (pos != -1) {
+		// 2021-09-16: Onigmo RegEx engine returns (-2) on invalid patterns
+		switch (pos) { // INVALID_REGEX, REGEX_ERROR & REGEX_EXCEPTION defined in Scintilla.h
+			case INVALID_REGEX: throw RegexCompileError();
+			case REGEX_ERROR: throw RegexRuntimeError();
+			case REGEX_EXCEPTION: throw RegexException();
+		}
+		///if (pos != INVALID_POSITION) {
+		if (pos < 0) {
 			ft->chrgText.cpMin = static_cast<Sci_PositionCR>(pos);
 			ft->chrgText.cpMax = static_cast<Sci_PositionCR>(pos + lengthFound);
 		}
 		return pos;
-	} catch (RegexError &) {
-		errorStatus = SC_STATUS_WARN_REGEX;
-		return -1;
+	}
+	catch (RegexCompileError &) {
+		errorStatus = SC_STATUS_WARN_REGEX_INVALID;
+		return INVALID_REGEX;
+	}
+	catch (RegexRuntimeError &) {
+		errorStatus = SC_STATUS_WARN_REGEX_ERROR;
+		return REGEX_ERROR;
+	}
+	catch (RegexException &) {
+		errorStatus = SC_STATUS_WARN_REGEX_EXCEPTION;
+		return REGEX_EXCEPTION;
 	}
 }
 
@@ -4112,9 +4128,18 @@ Sci::Position Editor::SearchText(
 					static_cast<int>(wParam),
 					&lengthFound);
 		}
-	} catch (RegexError &) {
-		errorStatus = SC_STATUS_WARN_REGEX;
-		return INVALID_POSITION;
+	}
+	catch (RegexCompileError &) {
+		errorStatus = SC_STATUS_WARN_REGEX_INVALID;
+		return INVALID_REGEX;
+	}
+	catch (RegexRuntimeError &) {
+		errorStatus = SC_STATUS_WARN_REGEX_INVALID;
+		return REGEX_ERROR;
+	}
+	catch (RegexException &) {
+		errorStatus = SC_STATUS_WARN_REGEX_EXCEPTION;
+		return REGEX_EXCEPTION;
 	}
 	if (pos != INVALID_POSITION) {
 		SetSelection(pos, pos + lengthFound);
@@ -4156,10 +4181,20 @@ Sci::Position Editor::SearchInTarget(const char *text, Sci::Position length) {
 			targetEnd = pos + lengthFound;
 		}
 		return pos;
-	} catch (RegexError &) {
-		errorStatus = SC_STATUS_WARN_REGEX;
-		return -1;
 	}
+	catch (RegexCompileError &) {
+		errorStatus = SC_STATUS_WARN_REGEX_INVALID;
+		return INVALID_REGEX;
+	}
+	catch (RegexRuntimeError &) {
+		errorStatus = SC_STATUS_WARN_REGEX_INVALID;
+		return REGEX_ERROR;
+	}
+	catch (RegexException &) {
+		errorStatus = SC_STATUS_WARN_REGEX_EXCEPTION;
+		return REGEX_EXCEPTION;
+	}
+
 }
 
 void Editor::GoToLine(Sci::Line lineNo) {
@@ -5914,6 +5949,25 @@ sptr_t Editor::WndProc(unsigned int iMessage, uptr_t wParam, sptr_t lParam) {
 			sel.selType = Selection::selStream;
 			SetSelection(nEnd, nStart);
 			EnsureCaretVisible();
+		}
+		break;
+
+	case SCI_GETREGEXLASTERROR: {
+			if (lParam == 0) {
+				return pdoc->GetRegexLastError().length();
+			}
+			else {
+				char *ptr = CharPtrFromSPtr(lParam);
+				size_t iChar = pdoc->GetRegexLastError().length();
+				if (iChar) {
+					memcpy(ptr, pdoc->GetRegexLastError().c_str(), iChar);
+					ptr[iChar++] = '\0';
+				}
+				else {
+					ptr[0] = '\0';
+				}
+				return iChar;
+			}
 		}
 		break;
 

scintilla/include/Scintilla.h
@@ -1134,6 +1134,15 @@ typedef sptr_t (*SciFnDirect)(sptr_t ptr, unsigned int iMessage, uptr_t wParam, 
 #endif
 /* --Autogenerated -- end of section automatically generated from Scintilla.iface */
 
+// 2021-09-17: Non-standard, manually added
+#define INVALID_REGEX -2
+#define REGEX_ERROR -3
+#define REGEX_EXCEPTION -4
+#define SC_STATUS_WARN_REGEX_INVALID 1002
+#define SC_STATUS_WARN_REGEX_ERROR 1003
+#define SC_STATUS_WARN_REGEX_EXCEPTION 1004
+#define SCI_GETREGEXLASTERROR 2999
+
 /* These structures are defined to be exactly the same shape as the Win32
  * CHARRANGE, TEXTRANGE, FINDTEXTEX, FORMATRANGE, and NMHDR structs.
  * So older code that treats Scintilla as a RichEdit will work. */

scintilla/src/Document.h
@@ -99,6 +99,8 @@ public:
 
 	///@return String with the substitutions, must remain valid until the next call or destruction
 	virtual const char *SubstituteByPosition(Document *doc, const char *text, Sci::Position *length) = 0;
+
+    virtual std::string GetErrorInfo() = 0;
 };
 
 /// Factory function for RegexSearchBase
@@ -187,8 +189,14 @@ public:
 	}
 };
 
-struct RegexError : public std::runtime_error {
-	RegexError() : std::runtime_error("regex failure") {}
+struct RegexCompileError : public std::runtime_error {
+	RegexCompileError() : std::runtime_error("regex compilation failure") {}
+};
+struct RegexRuntimeError : public std::runtime_error {
+	RegexRuntimeError() : std::runtime_error("regex execution failure") {}
+};
+struct RegexException : public std::runtime_error {
+	RegexException() : std::runtime_error("regex runtime exception") {}
 };
 
 /**
@@ -367,6 +375,8 @@ public:
 	bool TentativeActive() const { return cb.TentativeActive(); }
 
 	const char * SCI_METHOD BufferPointer() override { return cb.BufferPointer(); }
+    std::string SCI_METHOD GetRegexLastError() { return regex->GetErrorInfo(); }
+
 #ifdef X_SCINTILLA_RANGEGAP_FIX
 	const char *RangePointer(Sci::Position position, Sci::Position rangeLength) { return cb.DataPointer(position, rangeLength); }
 #else // !X_SCINTILLA_RANGEGAP_FIX

scintilla/src/Editor.h
@@ -475,6 +475,8 @@ protected:	// ScintillaBase subclass needs access to much of Editor
 
 	virtual CaseFolder *CaseFolderForEncoding();
 	Sci::Position FindText(uptr_t wParam, sptr_t lParam);
+    std::string GetRegexLastError() { return pdoc->GetRegexLastError(); }
+
 	void SearchAnchor();
 	Sci::Position SearchText(unsigned int iMessage, uptr_t wParam, sptr_t lParam);
 	Sci::Position SearchInTarget(const char *text, Sci::Position length);

scionigmo/OnigmoRegExEngine.cxx
@@ -174,6 +174,8 @@ public:
 
     const OnigRegion& GetRegion() const { return m_Region; };
 
+    std::string GetErrorInfo() override;
+
 private:
     std::string& translateRegExpr(std::string& regExprStr, bool wholeWord, bool wordStart, int eolMode, OnigOptionType& rxOptions);
 
@@ -280,12 +282,15 @@ Sci::Position OnigmoRegExEngine::FindText(Document* doc, Sci::Position minPos, S
  * Error return values:
  *  -1 => Not found
  *  -2 => Invalid regex
- *  -3 => Exception
+ *  -3 => ONIGMO-specific error
+ *  -4 => (Caught) Exception
+ *
+ * INVALID_REGEX, REGEX_ERROR & REGEX_EXCEPTION are defined in Scintilla.h
  */
 {
     if (!(pattern && (strlen(pattern) > 0))) {
         *length = 0;
-        return SciPos(-1);
+        return SciPos(INVALID_POSITION);
     }
 
     auto const docLen = SciPos(doc->Length());
@@ -342,10 +347,10 @@ Sci::Position OnigmoRegExEngine::FindText(Document* doc, Sci::Position minPos, S
                                m_CmplOptions, g_pOnigEncodingType, &m_OnigSyntax, &einfo);
             if (res != ONIG_NORMAL) {
                 onig_error_code_to_str(UCharPtr(m_ErrorInfo), res, &einfo);
-                return SciPos(-2); // -1 is normally used for not found, -2 is used here for invalid regex
+                return SciPos(INVALID_REGEX); // -1 is normally used for not found, -2 is used here for invalid regex
             }
         } catch (...) {
-            return SciPos(-2);
+            return SciPos(INVALID_REGEX);
         }
     }
 
@@ -382,12 +387,12 @@ Sci::Position OnigmoRegExEngine::FindText(Document* doc, Sci::Position minPos, S
         else
             result = onig_search(m_RegExpr, docBegPtr, docEndPtr, rangeEndPtr, rangeBegPtr, &m_Region, onigmoOptions);
     } catch (...) {
-        return SciPos(-3); // -1 is normally used for not found, -3 is used here for exception
+        return SciPos(REGEX_EXCEPTION); // -4 here is used here for an uncaught exception
     }
 
     if (result < ONIG_MISMATCH) {
         onig_error_code_to_str(UCharPtr(m_ErrorInfo), result);
-        return SciPos(-3);
+        return SciPos(REGEX_ERROR); // // -3 is used ONIGMO-specific errors
     }
 
     if ((result >= 0) && (rangeBegPtr <= rangeEndPtr)) {
@@ -402,6 +407,10 @@ Sci::Position OnigmoRegExEngine::FindText(Document* doc, Sci::Position minPos, S
 }
 // ============================================================================
 
+std::string OnigmoRegExEngine::GetErrorInfo() {
+	std::string sText(m_ErrorInfo, strlen(m_ErrorInfo));
+    return sText;
+}
 /*
 static int GrpNameCallback(const UChar* name, const UChar* name_end,
                            int ngroup_num, int* group_nums, regex_t* reg, void* arg)

src/Edit.c
@@ -5290,21 +5290,37 @@ BOOL EditFindNext(HWND hwnd, LPCEDITFINDREPLACE lpefr, BOOL fExtendSelection)
     ttf.chrg.cpMax = (int)SendMessage(hwnd, SCI_GETLENGTH, 0, 0);
     ttf.lpstrText = szFind2;
 
-    iPos = (int)SendMessage(hwnd, SCI_FINDTEXT, lpefr->fuFlags, (LPARAM)&ttf);
+    iPos = (int)SendMessage(hwnd, SCI_FINDTEXT, lpefr->fuFlags, (LPARAM)&ttf); // calls into Sci::Position Editor::FindText()
+    // 2021-09-17: Now handling other FindText() regex-specific return values: (-2), (-3) & (-4)
+    switch (iPos) {
+        case INVALID_POSITION: // Not found (-1)
+            if (ttf.chrg.cpMin > 0 && !lpefr->bNoFindWrap && !fExtendSelection) {
+                if (IDOK == InfoBox(MBOKCANCEL, L"MsgFindWrap1", IDS_FIND_WRAPFW)) {
+                    ttf.chrg.cpMin = 0;
+                    iPos = (int)SendMessage(hwnd, SCI_FINDTEXT, lpefr->fuFlags, (LPARAM)&ttf);
+                } else
+                    bSuppressNotFound = TRUE;
+            }
+            // notfound
+            if (!bSuppressNotFound)
+                InfoBox(0, L"MsgNotFound", IDS_NOTFOUND);
+            return FALSE;
 
-    if (iPos == -1 && ttf.chrg.cpMin > 0 && !lpefr->bNoFindWrap && !fExtendSelection) {
-        if (IDOK == InfoBox(MBOKCANCEL, L"MsgFindWrap1", IDS_FIND_WRAPFW)) {
-            ttf.chrg.cpMin = 0;
-            iPos = (int)SendMessage(hwnd, SCI_FINDTEXT, lpefr->fuFlags, (LPARAM)&ttf);
-        } else
-            bSuppressNotFound = TRUE;
-    }
+        case INVALID_REGEX: // Invalid regex pattern (-2)
+            MsgBox(MBWARN, IDS_FIND_REGEX_INVALID);
+            return FALSE;
 
-    if (iPos == -1) {
-        // notfound
-        if (!bSuppressNotFound)
-            InfoBox(0, L"MsgNotFound", IDS_NOTFOUND);
-        return FALSE;
+        case REGEX_ERROR: { // Regex runtime error (-3)
+            int cchErrInfo = (int)SendMessage(hwnd, SCI_GETREGEXLASTERROR, 0, 0) - 1;
+            char* pszErrInfo = LocalAlloc(LPTR, cchErrInfo + 1);
+            (int)SendMessage(hwnd, SCI_GETREGEXLASTERROR, 0, (LPARAM)pszErrInfo);
+            MsgBox(MBWARN, IDS_FIND_REGEX_ONIGERR, pszErrInfo);
+            return FALSE;
+        }
+
+        case (REGEX_EXCEPTION): // Regex exception (-4)
+            MsgBox(MBFATAL, IDS_FIND_REGEX_EXCEPTN);
+            return FALSE;
     }
 
     if (!fExtendSelection)
@@ -5367,11 +5383,30 @@ BOOL EditFindPrev(HWND hwnd, LPCEDITFINDREPLACE lpefr, BOOL fExtendSelection)
             bSuppressNotFound = TRUE;
     }
 
-    if (iPos == -1) {
-        // notfound
+    if (iPos == INVALID_POSITION) { // Not found (-1)
         if (!bSuppressNotFound)
             InfoBox(0, L"MsgNotFound", IDS_NOTFOUND);
         return FALSE;
+    }
+    else if (iPos < 0) {
+        // 2021-09-17: Check if we had regex-specific error return values
+        switch (iPos) {
+            case INVALID_REGEX: // Invalid regex pattern (-2)
+                MsgBox(MBWARN, IDS_FIND_REGEX_INVALID);
+                return FALSE;
+
+            case REGEX_ERROR: { // Regex runtime error (-3)
+                int cchErrInfo = (int)SendMessage(hwnd, SCI_GETREGEXLASTERROR, 0, 0) - 1;
+                char* pszErrInfo = LocalAlloc(LPTR, cchErrInfo + 1);
+                (int)SendMessage(hwnd, SCI_GETREGEXLASTERROR, 0, (LPARAM)pszErrInfo);
+                MsgBox(MBWARN, IDS_FIND_REGEX_ONIGERR, pszErrInfo);
+                return FALSE;
+            }
+
+            case REGEX_EXCEPTION: // Regex exception (-4)
+                MsgBox(MBFATAL, IDS_FIND_REGEX_EXCEPTN);
+                return FALSE;
+        }
     }
 
     if (!fExtendSelection)
@@ -5454,7 +5489,7 @@ BOOL EditReplace(HWND hwnd, LPCEDITFINDREPLACE lpefr)
 
     // If we reached the end of the document and are allowed to wrap around, do so
     // with a new "find" operation
-    if (iPos == -1 && ttf.chrg.cpMin > 0 && !lpefr->bNoFindWrap) {
+    if (iPos == INVALID_POSITION && ttf.chrg.cpMin > 0 && !lpefr->bNoFindWrap) {
         if (IDOK == InfoBox(MBOKCANCEL, L"MsgFindWrap1", IDS_FIND_WRAPFW)) {
             ttf.chrg.cpMin = 0;
             iPos = (int)SendMessage(hwnd, SCI_FINDTEXT, lpefr->fuFlags, (LPARAM)&ttf);
@@ -5463,12 +5498,31 @@ BOOL EditReplace(HWND hwnd, LPCEDITFINDREPLACE lpefr)
     }
 
     // Check if the Scintilla edit came up empty-handed
-    if (iPos == -1) {
+    if (iPos == INVALID_POSITION) { // Not found (-1)
         // Search string not found; clean up and return
         LocalFree(pszReplace2);
         if (!bSuppressNotFound)
             InfoBox(0, L"MsgNotFound", IDS_NOTFOUND);
         return FALSE;
+    } else if (iPos < 0) {
+        // 2021-09-17: Check if we had regex-specific error return values
+        switch (iPos) {
+            case INVALID_REGEX: // Invalid regex pattern (-2)
+                MsgBox(MBWARN, IDS_FIND_REGEX_INVALID);
+                return FALSE;
+
+            case REGEX_ERROR: { // Regex runtime error (-3)
+                int cchErrInfo = (int)SendMessage(hwnd, SCI_GETREGEXLASTERROR, 0, 0) - 1;
+                char* pszErrInfo = LocalAlloc(LPTR, cchErrInfo + 1);
+                (int)SendMessage(hwnd, SCI_GETREGEXLASTERROR, 0, (LPARAM)pszErrInfo);
+                MsgBox(MBWARN, IDS_FIND_REGEX_ONIGERR, pszErrInfo);
+                return FALSE;
+            }
+
+            case REGEX_EXCEPTION: // Regex exception (-4)
+                MsgBox(MBFATAL, IDS_FIND_REGEX_EXCEPTN);
+                return FALSE;
+        }
     }
 
     // Make sure 
@@ -5488,7 +5542,7 @@ BOOL EditReplace(HWND hwnd, LPCEDITFINDREPLACE lpefr)
     iPos = (int)SendMessage(hwnd, SCI_FINDTEXT, lpefr->fuFlags, (LPARAM)&ttf);
 
     bSuppressNotFound = FALSE;
-    if (iPos == -1 && ttf.chrg.cpMin > 0 && !lpefr->bNoFindWrap) {
+    if (iPos == INVALID_POSITION && ttf.chrg.cpMin > 0 && !lpefr->bNoFindWrap) {
         if (IDOK == InfoBox(MBOKCANCEL, L"MsgFindWrap1", IDS_FIND_WRAPFW)) {
             ttf.chrg.cpMin = 0;
             iPos = (int)SendMessage(hwnd, SCI_FINDTEXT, lpefr->fuFlags, (LPARAM)&ttf);
@@ -5496,7 +5550,7 @@ BOOL EditReplace(HWND hwnd, LPCEDITFINDREPLACE lpefr)
             bSuppressNotFound = TRUE;
     }
 
-    if (iPos != -1)
+    if (iPos != INVALID_POSITION)
         EditSelectEx(hwnd, ttf.chrgText.cpMin, ttf.chrgText.cpMax);
 
     else {
@@ -5715,13 +5769,34 @@ void EditMarkAll(HWND hwnd, int iMarkOccurrences, BOOL bMarkOccurrencesMatchCase
     while ((iPos = (int)SendMessage(hwnd, SCI_FINDTEXT,
         (bMarkOccurrencesMatchCase ? SCFIND_MATCHCASE : 0) | (bMarkOccurrencesMatchWords ? SCFIND_WHOLEWORD : 0),
                                     (LPARAM)&ttf))
-           != -1
+           >= 0
            && ++iMatchesCount < 2000) {
         // mark this match
         SendMessage(hwnd, SCI_INDICATORFILLRANGE, iPos, iSelCount);
         ttf.chrg.cpMin = ttf.chrgText.cpMin + iSelCount;
         if (ttf.chrg.cpMin == ttf.chrg.cpMax)
             break;
+    }
+
+    // 2021-09-17: Check if we had regex-specific error return values
+    if (iPos < 0) {
+        switch (iPos) {
+            case INVALID_REGEX: // Invalid regex pattern (-2)
+                MsgBox(MBWARN, IDS_FIND_REGEX_INVALID);
+                return;
+
+            case REGEX_ERROR: { // Regex runtime error (-3)
+                int cchErrInfo = (int)SendMessage(hwnd, SCI_GETREGEXLASTERROR, 0, 0) - 1;
+                char* pszErrInfo = LocalAlloc(LPTR, cchErrInfo + 1);
+                (int)SendMessage(hwnd, SCI_GETREGEXLASTERROR, 0, (LPARAM)pszErrInfo);
+                MsgBox(MBWARN, IDS_FIND_REGEX_ONIGERR, pszErrInfo);
+                return;
+            }
+
+            case REGEX_EXCEPTION: // Regex exception (-4)
+                MsgBox(MBFATAL, IDS_FIND_REGEX_EXCEPTN);
+                return;
+        }
     }
 
     LocalFree(pszText);
@@ -5788,7 +5863,8 @@ BOOL EditReplaceAll(HWND hwnd, LPCEDITFINDREPLACE lpefr, BOOL bShowInfo)
     ttf.chrg.cpMax = (int)SendMessage(hwnd, SCI_GETLENGTH, 0, 0);
     ttf.lpstrText = szFind2;
 
-    while ((iPos = (int)SendMessage(hwnd, SCI_FINDTEXT, lpefr->fuFlags, (LPARAM)&ttf)) != -1) {
+    // 2021-09-17: Now handling other FindText() regex-specific return values
+    while ((iPos = (int)SendMessage(hwnd, SCI_FINDTEXT, lpefr->fuFlags, (LPARAM)&ttf)) >= 0) {
         int iReplacedLen;
         //char ch;
 
@@ -5837,8 +5913,29 @@ BOOL EditReplaceAll(HWND hwnd, LPCEDITFINDREPLACE lpefr, BOOL bShowInfo)
     if (bShowInfo) {
         if (iCount > 0)
             InfoBox(0, L"MsgReplaceCount", IDS_REPLCOUNT, iCount);
-        else
-            InfoBox(0, L"MsgNotFound", IDS_NOTFOUND);
+        else {
+            // 2021-09-17: Check if we had regex-specific error return values
+            switch (iPos) {
+                case INVALID_REGEX: // Invalid regex pattern (-2)
+                    MsgBox(MBWARN, IDS_FIND_REGEX_INVALID);
+                    return FALSE;
+
+                case REGEX_ERROR: { // Regex runtime error (-3)
+                    int cchErrInfo = (int)SendMessage(hwnd, SCI_GETREGEXLASTERROR, 0, 0) - 1;
+                    char* pszErrInfo = LocalAlloc(LPTR, cchErrInfo + 1);
+                    (int)SendMessage(hwnd, SCI_GETREGEXLASTERROR, 0, (LPARAM)pszErrInfo);
+                    MsgBox(MBWARN, IDS_FIND_REGEX_ONIGERR, pszErrInfo);
+                    return FALSE;
+                }
+
+                case REGEX_EXCEPTION: // Regex exception (-4)
+                    MsgBox(MBFATAL, IDS_FIND_REGEX_EXCEPTN);
+                    return FALSE;
+
+                default:
+                    InfoBox(0, L"MsgNotFound", IDS_NOTFOUND);
+            }
+        }
     }
 
     LocalFree(pszReplace2);
@@ -5981,8 +6078,29 @@ BOOL EditReplaceAllInSelection(HWND hwnd, LPCEDITFINDREPLACE lpefr, BOOL bShowIn
     if (bShowInfo) {
         if (iCount > 0)
             InfoBox(0, L"MsgReplaceCount", IDS_REPLCOUNT, iCount);
-        else
-            InfoBox(0, L"MsgNotFound", IDS_NOTFOUND);
+        else {
+            // 2021-09-17: Check if we had regex-specific error return values
+            switch (iPos) {
+                case INVALID_REGEX: // Invalid regex pattern (-2)
+                    MsgBox(MBWARN, IDS_FIND_REGEX_INVALID);
+                    return FALSE;
+
+                case REGEX_ERROR: { // Regex runtime error (-3)
+                    int cchErrInfo = (int)SendMessage(hwnd, SCI_GETREGEXLASTERROR, 0, 0) - 1;
+                    char* pszErrInfo = LocalAlloc(LPTR, cchErrInfo + 1);
+                    (int)SendMessage(hwnd, SCI_GETREGEXLASTERROR, 0, (LPARAM)pszErrInfo);
+                    MsgBox(MBWARN, IDS_FIND_REGEX_ONIGERR, pszErrInfo);
+                    return FALSE;
+                }
+
+                case REGEX_EXCEPTION: // Regex exception (-4)
+                    MsgBox(MBFATAL, IDS_FIND_REGEX_EXCEPTN);
+                    return FALSE;
+
+                default:
+                    InfoBox(0, L"MsgNotFound", IDS_NOTFOUND);
+            }
+        }
     }
 
     LocalFree(pszReplace2);

src/Notepad2.rc
@@ -1917,6 +1917,9 @@ BEGIN
     IDS_BTN_DONTSAVE        "Do&n't Save"
 #endif
 #endif // FEAT_REPLACE_MSGBOX_BY_TASKDLG
+    IDS_FIND_REGEX_INVALID  "The search pattern is not a valid regular expression.\nCheck and fix pattern before trying again."
+    IDS_FIND_REGEX_ONIGERR  "The regex engine (Onigmo) returned the following error: ""%s""."
+    IDS_FIND_REGEX_EXCEPTN  "An exception occured while compiling or executing the regular exception. No further details are available."
 END
 
 #ifdef JRB_BUILD

src/resource.h
@@ -529,6 +529,9 @@
 #define IDS_BTN_DONTSAVE                50063
 #endif // JRB_BUILD
 #endif // FEAT_REPLACE_MSGBOX_BY_TASKDLG
+#define IDS_FIND_REGEX_INVALID          50070
+#define IDS_FIND_REGEX_ONIGERR          50071
+#define IDS_FIND_REGEX_EXCEPTN          50072
 #ifdef JRB_BUILD
 #define IDS_CTLCHAR_00_TOOLTIP          50100
 #define IDS_CTLCHAR_01_TOOLTIP          50101

commit f0bc1b14c04325a2750be8526d0671aaa7b8f637

Fixed a logic bug in Editor.cxx introduced by cc900a6...
... mostly showing up when "Mark Occurrences" is on (but that would also affect find/replace operations) discarding every occurrence being found.

build/make_zip.bat
@@ -32,7 +32,7 @@ SET "l_readme3_file=Readme.txt"
 SET "l_license_path=..\"
 SET "l_license_file=License.txt"
 SET "l_info_path=..\"
-SET "l_info_file=Notepad2.txt"
+SET "l_info_file=Notepad2-mod-jrb.txt"
 SET "l_config_path=..\distrib\"
 SET "l_config_file=Notepad2.ini"
 SET "l_zip_everything=1"

distrib/notepad2_setup.iss
@@ -13,7 +13,7 @@
 #define ProjectName              "Notepad2-mod-jrb"
 #define ProjectHead              "Jonathan R.-Brochu"
 #define ProjectRepo              "https://github.com/johnwait/notepad2-mod-jrb"
-#define CopyrightNotice          "Copyright © 2004-2017, Florian Balmer et al.; Copyright © 2010-2017 XhmikosR; Copyright © 2019-2020" + ProjectHead + " et al."
+#define CopyrightNotice          "Copyright © 2004-2017, Florian Balmer et al.; Copyright © 2010-2017 XhmikosR; Copyright © 2019-2021" + ProjectHead + " et al."
 #define AppTaskbarGrpId          "Notepad2"
 ;
 #define Files_MainBinary         "Notepad2-jrb.exe"

scintilla/src/Editor.cxx
@@ -4067,7 +4067,7 @@ Sci::Position Editor::FindText(
 			case REGEX_EXCEPTION: throw RegexException();
 		}
 		///if (pos != INVALID_POSITION) {
-		if (pos < 0) {
+		if (pos >= 0) {
 			ft->chrgText.cpMin = static_cast<Sci_PositionCR>(pos);
 			ft->chrgText.cpMax = static_cast<Sci_PositionCR>(pos + lengthFound);
 		}